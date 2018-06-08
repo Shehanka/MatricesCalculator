df70f10c1d2d7b4b26f726479bf268341ac0be4e
Shehanka <hcsperera@outlook.com>
2018-06-09 00:19:55 +0530

    main Class created

---
+++ src/com/chamodshehanka/matricesCalculator/main/MatricesCalculator.java
@@ -0,0 +1,16 @@
+package com.chamodshehanka.matricesCalculator.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author chamodshehanka on 6/9/2018
+ * @project MatricesCalculator
+ **/
+public class MatricesCalculator extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
