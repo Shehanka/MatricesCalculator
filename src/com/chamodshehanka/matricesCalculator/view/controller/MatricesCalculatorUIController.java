6c26b188dadf72561e2aff5aed76c1f22cbc6c68
Shehanka <hcsperera@outlook.com>
2018-06-09 00:23:08 +0530

    First UI Created

---
+++ src/com/chamodshehanka/matricesCalculator/view/controller/MatricesCalculatorUIController.java
@@ -0,0 +1,18 @@
+package com.chamodshehanka.matricesCalculator.view.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 6/9/2018
+ * @project MatricesCalculator
+ **/
+public class MatricesCalculatorUIController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
