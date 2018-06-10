ed448f985e2861ca364cb42710e060b08b88496c
Shehanka <hcsperera@outlook.com>
2018-06-10 16:18:05 +0530

    main class

--- src/com/chamodshehanka/matricesCalculator/main/MatricesCalculator.java
+++ src/com/chamodshehanka/matricesCalculator/main/MatricesCalculator.java
@@ -13,4 +13,8 @@ public class MatricesCalculator extends Application{
     public void start(Stage primaryStage) throws Exception {
 
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
