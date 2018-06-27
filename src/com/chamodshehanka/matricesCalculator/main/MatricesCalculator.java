edc29073b8e96f90b08bc946ece209a143756fdf
Shehanka <hcsperera@outlook.com>
2018-06-27 17:56:10 +0530

    Project Icon added

--- src/com/chamodshehanka/matricesCalculator/main/MatricesCalculator.java
+++ src/com/chamodshehanka/matricesCalculator/main/MatricesCalculator.java
@@ -1,6 +1,10 @@
 package com.chamodshehanka.matricesCalculator.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +15,13 @@ public class MatricesCalculator extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent parent = FXMLLoader.load(getClass()
+                .getResource("/com/chamodshehanka/matricesCalculator/view/fxml/MatricesCalculatorUI.fxml"));
+        primaryStage.setTitle("Matrices Calculator");
+        primaryStage.getIcons().add(new Image(getClass()
+                .getResourceAsStream("/com/chamodshehanka/matricesCalculator/icon/icon.png")));
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
